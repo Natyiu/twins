f0143713967f3801360b6ed92b3d94dd5490a188
mac <mac@macs-MacBook-Pro.local>
2026-04-06 13:50:49 +0300

    working on anlytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import App from './App.jsx'
+import { PostHogProvider } from '@posthog/react'
 
-createRoot(document.getElementById('root')!).render(
+const options = {
+  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+  defaults: '2026-01-30',
+} as const
+
+createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <PostHogProvider apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_PROJECT_TOKEN} options={options}>
+      <App />
+    </PostHogProvider>
+  </StrictMode>
 )
